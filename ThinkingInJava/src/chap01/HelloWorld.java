e5e190bf84a5265be222d0275c0fa5b6150edf43
sun <sunyujun16@163.com>
2021-03-12 21:25:24 +0800

    📅📅2021/3/12 first commit by sunyujun.

---
+++ ThinkingInJava/src/chap01/HelloWorld.java
@@ -0,0 +1,8 @@
+// chap01/HelloWorld
+package chap01;
+
+public class HelloWorld {
+    public static void main(String[] arg){
+        System.out.println("Hello, Java world, I'll conquer you!");
+    }
+}
---
+++ ThinkingInJava/src/chap02_Object/HelloDate.java
@@ -0,0 +1,11 @@
+// chap02_Object/HelloDate
+
+package chap02_Object;
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it;s: ");
+        System.out.println(new Date());
+    }
+}
