6c00e3d32e39e9447905dac52f397242ca904bea
sun <sunyujun16@163.com>
2021-03-20 18:37:41 +0800

    code everyday, job on its way!

--- ThinkingInJava/src/chap07_ReusingClasses/E02_Inheritance01.java
+++ ThinkingInJava/src/chap07_ReusingClasses/E02_Inheritance01.java
@@ -2,6 +2,7 @@ package chap07_ReusingClasses;
 
 class Father {
     private String s;
+    String j = "J is not private.";
 
     Father() {
         s = "Daddy.";
@@ -18,6 +19,10 @@ class Father {
 
     void move() {
         append(" father walk.");
+    }
+
+    void dispose(){
+        System.out.println("Fa dispose.");
     }
 
     @Override
@@ -37,6 +42,8 @@ class Father {
 class Son extends Father {
     Son() {
         System.out.println("Son is ready!");
+//        super();
+//        Error:(40, 14) java: 对super的调用必须是构造器中的第一个语句
     }
 
     @Override
@@ -49,6 +56,20 @@ class Son extends Father {
         append(" son run.");
     }
 
+    @Override
+    void dispose(){
+        System.out.println("Son dispose.");
+        super.dispose();
+    }
+
+    void getJ() {
+        System.out.println(this.j + "says Son.");
+    }
+
+    void getJ(int n) {
+        System.out.println(this.j + "says Son. param: " + n);
+    }
+
     public static void main(String[] args) {
         Son s1 = new Son();
         s1.append(" sonTest.");
@@ -59,7 +80,7 @@ class Son extends Father {
 }
 
 class Grand extends Son {
-    Grand(){
+    Grand(int n) {
         System.out.println("Grand is ready!");
     }
 
@@ -73,18 +94,45 @@ class Grand extends Son {
         append(" grandson crawl.");
     }
 
-    void study() {
+    private void study() {
         append(" go to school.");
     }
 
+    // 在子类重载父类的方法, 而非覆写.
+    /** @noinspection SameParameterValue, WeakerAccess */
+    void getJ(int n, String s) {
+        System.out.println(this.j + "says Grand. param: " + n);
+        super.getJ(n);
+    }
+
+    @Override
+    void dispose(){
+        System.out.println("Grand dispose.");
+        super.dispose();
+    }
+
     public static void main(String[] args) {
-        Grand g1 = new Grand();
+        Grand g1 = new Grand(8);
         g1.append(" grandTest");
         g1.eat();
         g1.move();
         g1.study();
+        g1.getJ(8, "a");
         System.out.println(g1);
     }
+}
+
+class Baby extends Grand {
+    Baby(int n) {
+        super(n);
+        System.out.println("Wa~~~~~~~, wa~~~~~~");
+    }
+}
+
+class Baby2{
+    public Baby2() {
+    }
+
 }
 
 public class E02_Inheritance01 {
@@ -93,8 +141,12 @@ public class E02_Inheritance01 {
 //        Grand grand = new Grand();
 //        System.out.println("----------------------------");
 
-        /* 实际上, 这种使用方式, 等于通过子类的实例, 去间接操纵父类的实例(并不实际存在),
-        * 从而操作子类不具备的(不能直接访问的)字段和方法. */
+        Baby baby = new Baby(8);
+        System.out.println(baby);
+        System.out.println("----------------------------");
+
+        /* 实际上, 这种使用方式, 等于通过子类的实例, 去间接操纵父类的实例(被包在子类实例内部),
+         * 从而操作子类不具备的(不能直接访问的)字段和方法. */
         Grand.main(args);
         System.out.println("----------------------------");
 
@@ -102,6 +154,8 @@ public class E02_Inheritance01 {
         System.out.println("----------------------------");
 
         Father.main(args);
+        System.out.println("----------------------------");
 
+        baby.dispose();
     }
 }
