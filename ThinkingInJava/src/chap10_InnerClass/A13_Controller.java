853db01e712221f22bf8fceb3055c708b30c4ce8
sun <sunyujun16@163.com>
2021-03-25 21:02:43 +0800

    code everyday, job on its way!

--- ThinkingInJava/src/chap10_InnerClass/A13_Controller.java
+++ ThinkingInJava/src/chap10_InnerClass/A13_Controller.java
@@ -1,11 +1,15 @@
 package chap10_InnerClass;
 
 // The reusable framework for control systems
+//import java.sql.Time;
+
 import java.util.*;
 import java.time.*;
 
 public class A13_Controller {
     // A class from java.util to hold A12_Event objects:
+    private static final Instant ins = Instant.now();
+    String sThen = (ins.toString().split(":")[2].substring(0, 5));
     private List<A12_Event> eventList = new ArrayList<>();
 
     public void addA12_Event(A12_Event c) {
@@ -19,7 +23,11 @@ public class A13_Controller {
             for (A12_Event e : new ArrayList<>(eventList))
                 if (e.ready()) {
 //                    System.out.println(e);
-                    System.out.println(e + " --- " + Instant.now());
+                    String sNow = (Instant.now().toString().split(":")[2].substring(0, 5));
+                    double timeMinus = Double.parseDouble(sNow) - Double.parseDouble(sThen);
+//                    double f1 = timeMinus.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+                    String res = String.format("%.2f", timeMinus);
+                    System.out.println(res + " --- " + e);
                     e.action();
                     eventList.remove(e);
                 }
