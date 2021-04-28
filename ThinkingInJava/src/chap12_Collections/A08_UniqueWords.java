ba871c27114a6ffdee3b892955c180bd8786551e
sun <sunyujun16@163.com>
2021-04-28 22:49:47 +0800

    code everyday, job on its way!

--- ThinkingInJava/src/chap12_Collections/A08_UniqueWords.java
+++ ThinkingInJava/src/chap12_Collections/A08_UniqueWords.java
@@ -3,25 +3,25 @@ package chap12_Collections;
 import java.util.*;
 import java.nio.file.*;
 
+// 练习16修改版.
 public class A08_UniqueWords {
     public static void main(String[] args) throws Exception {
 
         List<String> lines = Files.readAllLines(
-                Paths.get("/home/sun/IdeaProjects/JavaLearningNotes" +
-                        "/ThinkingInJava/src/chap12_Collections/A08_SetOperations.java")
+                Paths.get("ThinkingInJava/src/chap12_Collections/A08_SetOperations.java")
         );
 
 //        Set<String> words = new TreeSet<>();
         Set<String> words = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
 
-        Set<Character> vowels = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         int sum = 0;
         int num = 0;
 
         for (String line : lines)
             for (String word : line.split("\\W+")) {
 
-                List<Character> chars = new ArrayList<>();
+//                List<Character> chars = new ArrayList<>();
                 for (Character c : vowels) {
                     for (Character w : word.toCharArray()) {
                         if (c == w) num++;
