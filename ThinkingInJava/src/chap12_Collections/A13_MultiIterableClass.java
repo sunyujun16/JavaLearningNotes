117ec5e6ff7d07fd25156618d9bdd672e8f44289
sun <sunyujun16@163.com>
2021-04-29 19:15:16 +0800

    code everyday, job on its way!

--- ThinkingInJava/src/chap12_Collections/A13_MultiIterableClass.java
+++ ThinkingInJava/src/chap12_Collections/A13_MultiIterableClass.java
@@ -6,47 +6,39 @@ import java.util.*;
 
 public class A13_MultiIterableClass extends A13_IterableClass {
     public Iterable<String> reversed() {
-        return new Iterable<String>() {
-            public Iterator<String> iterator() {
-                return new Iterator<String>() {
-                    int current = words.length - 1;
+        return () -> new Iterator<>() {
+            int current = words.length - 1;
 
-                    public boolean hasNext() {
-                        return current > -1;
-                    }
+            public boolean hasNext() {
+                return current > -1;
+            }
 
-                    public String next() {
-                        return words[current--];
-                    }
-
-                    public void remove() { // Not implemented
-                        throw new UnsupportedOperationException();
-                    }
-                };
+            public String next() {
+                return words[current--];
             }
         };
     }
 
     public Iterable<String> randomized() {
-        return new Iterable<String>() {
-            public Iterator<String> iterator() {
-                List<String> shuffled =
-                        new ArrayList<String>(Arrays.asList(words));
-                Collections.shuffle(shuffled, new Random(47));
-                // 调用List自带的iterator方法.
-                return shuffled.iterator();
-            }
+        return () -> {
+            List<String> shuffled =
+                    new ArrayList<>(Arrays.asList(words));
+            Collections.shuffle(shuffled, new Random(47));
+            return shuffled.iterator();
         };
     }
 
     public static void main(String[] args) {
         A13_MultiIterableClass mic = new A13_MultiIterableClass();
+
         for (String s : mic.reversed())
             System.out.print(s + " ");
         System.out.println();
+
         for (String s : mic.randomized())
             System.out.print(s + " ");
         System.out.println();
+
         for (String s : mic)
             System.out.print(s + " ");
     }
