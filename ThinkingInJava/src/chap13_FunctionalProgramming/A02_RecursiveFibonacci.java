7a644d14be2db32a02b310b9c4cff2582b9a8b57
sun <sunyujun16@163.com>
2021-04-30 16:58:44 +0800

    code everyday, job on its way!

--- ThinkingInJava/src/chap13_FunctionalProgramming/A02_RecursiveFibonacci.java
+++ ThinkingInJava/src/chap13_FunctionalProgramming/A02_RecursiveFibonacci.java
@@ -1,7 +1,11 @@
 package chap13_FunctionalProgramming;
 
+interface IntCall {
+    int call(int n, int a, int b);
+}
+
 public class A02_RecursiveFibonacci {
-    A03_IntCall fib;
+    IntCall fib;
 
     // 我的做法
 //    public static void main(String[] args) {
@@ -12,14 +16,14 @@ public class A02_RecursiveFibonacci {
 //        System.out.println(ar.fib.call(10));
 //    }
 
-    // 下面是书里的做法, 可以看作是对我的代码的重构, 以提供复用性.
+    // 下面是书里的做法, 可以看作是对我的-代码的重构, 以提供复用性.
+    // 改为尾递归.
     A02_RecursiveFibonacci() {
-        fib = n -> n == 0 ? 0 :
-                n == 1 ? 1 :
-                        fib.call(n - 1) + fib.call(n - 2);
+        fib = (n, a, b) -> n == 0 ? a :
+                n == 1 ? b : fib.call(n-1, b, a+b);
     }
 
-    int fibonacci(int n) { return fib.call(n); }
+    int fibonacci(int n) { return fib.call(n, 0, 1); }
 
     public static void main(String[] args) {
         A02_RecursiveFibonacci rf = new A02_RecursiveFibonacci();
