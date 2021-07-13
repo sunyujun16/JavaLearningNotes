eb2065d5ae78d243fe3fb1c0f53bf3fb8df13029
sun <sunyujun16@163.com>
2021-07-13 21:26:21 +0800

    code everyday, job on its way!

--- ThinkingInJava/src/chap14_streams/A03_SortedComparator.java
+++ ThinkingInJava/src/chap14_streams/A03_SortedComparator.java
@@ -1,16 +1,20 @@
 package chap14_streams;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class A03_SortedComparator {
-    public static void
-    main(String[] args) throws Exception {
-        A03_FileToWords.stream("ThinkingInJava/src/chap14_streams/A02_Cheese.txt")
+    public static void main(String[] args) throws Exception {
+        A03_FileToWords.stream("ThinkingInJava/src/" +
+                "chap14_streams/A02_Cheese.txt")
                 .skip(10)
                 .limit(10)
                 .sorted(Comparator.reverseOrder())
                 .map(w -> w + " ")
                 .forEach(System.out::print);
+
+//        IntStream.range(1, 6)
+//                .mapToObj(k -> 0);
     }
 }
 /* Output:
